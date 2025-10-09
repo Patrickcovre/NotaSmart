3b11bf2f094e70037b43979768cdc80f3d14126a
Patrick Covre <covrepatrick1@gmail.com>
2025-10-08 22:44:22 -0300

    feat new function, insert prompt in chat

---
+++ main.py
@@ -0,0 +1,7 @@
+def meu_input():
+    ab = input("Digite")
+    return ab
+
+v = meu_input()
+
+print(v)
---
+++ services/chat_prompt.py
@@ -0,0 +1,3 @@
+def chat_for_prompt():
+    my_prompt = input(str("Digite Seu prompt: "))
+    return my_prompt
