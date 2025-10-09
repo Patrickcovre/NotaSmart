203239485d2723fa82a9dd34fdb0685aa711b782
Patrick Covre <covrepatrick1@gmail.com>
2025-10-08 21:48:51 -0300

    feat - config secrets in core/config.py

---
+++ core/config.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    def __init__(self):
+        self.openai_api_key = os.getenv("OPENAI_API_KEY")
+        self.openai_model = os.getenv("OPENAI_MODEL")
+config = Config()
