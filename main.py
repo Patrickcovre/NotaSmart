fa175215e72a752236606ed5c0a0bba678904233
Patrick Covre <covrepatrick1@gmail.com>
2025-10-08 21:12:08 -0300

    feat - new method openai

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-#Aqui é um comeco
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+client = OpenAI()
+
+response = client.responses.create(
+    model="gpt-5",
+    input="Voce me escuta? se sim quantos dolares isto vai me custar?."
+)
+
+print(response.output_text)
