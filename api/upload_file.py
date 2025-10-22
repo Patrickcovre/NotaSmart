02ea1f11a2e75d38674478787a9e82e2645dba66
Patrick Covre <covrepatrick1@gmail.com>
2025-10-21 22:25:15 -0300

    Funcionalidade de upload de arquivos funcionado

--- api/upload_file.py
+++ api/upload_file.py
@@ -6,4 +6,4 @@ app = FastAPI()
 @app.post("/upload_file")
 async def upload_file(file: List[UploadFile]):
     #Usar UploadFile para se caso a memória estiver quase estourando ele vai passar para o disco e continuar o processo
-    return {"filename": file.filename}
+    return UploadFile(file)
