36de5400bbd55eef96381a41701898f8d2406986
Patrick Covre <covrepatrick1@gmail.com>
2025-10-21 23:09:43 -0300

    feat - Estou comecando a criar tipos de arquivos que o sistema pode aceitar

--- api/upload_file.py
+++ api/upload_file.py
@@ -1,9 +1,12 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, HTTPException
 from typing import List
 
 app = FastAPI()
 
 @app.post("/upload_file")
-async def upload_file(file: List[UploadFile]):
+async def upload_file(file:UploadFile):
     #Usar UploadFile para se caso a memória estiver quase estourando ele vai passar para o disco e continuar o processo
-    return UploadFile(file)
+    if file.content_type != "image/jpeg":
+        raise HTTPException(status_code=400, detail="File must be a JPEG image")
+
+    return {"filename": file}
